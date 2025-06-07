9404555d9f5e50f1f85a86d8f1089b99e54849e1
LUDOVIC SCELLES <ludovic.scelles@gmail.com>
2025-06-07 15:11:34 +0200

    index6 : revision ex numberOfDigits and reverseAString

---
+++ index6.ts
@@ -0,0 +1,57 @@
+// ******
+
+import { string } from "joi";
+
+// 1. Nombre de chiffres
+// Écris une fonction qui retourne le nombre de chiffres dans un entier donné.
+
+// Exemple : 12345 → 5
+
+// ******
+
+function numberOfDigits(nb: number): number {
+  if (!Number.isInteger(nb)) {
+    throw new Error(`La saisie doit être un nombre entier`);
+  }
+
+  if (nb === 0) {
+    return 1;
+  }
+
+  nb = Math.abs(nb);
+
+  let count: number = 0;
+
+  for (let i = nb; i > 0; i = Math.floor(i / 10)) {
+    count++;
+  }
+  return count;
+}
+
+console.log(numberOfDigits(14546513));
+console.log(numberOfDigits(0));
+console.log(numberOfDigits(-61));
+
+// ***********
+
+// 2. Inverser une chaîne
+// Écris une fonction qui retourne une chaîne inversée.
+
+// Exemple : "bonjour" → "ruojnob"
+
+// ************
+
+function reverseAString(str: string): string {
+  if (typeof str !== "string") {
+    throw new Error("La saisie doit être une chaîne de caractères");
+  }
+
+  let newString: string = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    newString += str[i];
+  }
+  return newString;
+}
+
+console.log(reverseAString("Ludovic"));
