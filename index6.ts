c1a758e757db2a0961783546d09ea204b2ea8822
LUDOVIC SCELLES <ludovic.scelles@gmail.com>
2025-06-09 09:59:55 +0200

    index6 : revision functions primeNumber and countLetters

--- index6.ts
+++ index6.ts
@@ -1,7 +1,6 @@
 // ******
 
-import { error } from "console";
-import { func, string } from "joi";
+import { string } from "joi";
 
 // 1. Nombre de chiffres
 // Écris une fonction qui retourne le nombre de chiffres dans un entier donné.
@@ -99,3 +98,63 @@ function sumOfDigits(nbr: number): number {
 
 console.log(sumOfDigits(1515));
 console.log(sumOfDigits(-15));
+
+// 5. Nombre premier
+// Écris une fonction qui vérifie si un nombre est premier (divisible uniquement par 1 et lui-même).
+
+// **************************
+
+function primeNumber(nbr: number): boolean {
+  if (!Number.isInteger(nbr)) {
+    throw new Error("La saisie doit être un nombre entier");
+  } else if (nbr <= 1) {
+    return false;
+  } else {
+    for (let i = 2; i <= Math.sqrt(nbr); i++) {
+      if (nbr % i === 0) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+console.log(primeNumber(2));
+console.log(primeNumber(3));
+console.log(primeNumber(4));
+
+// 6. Compter les lettres
+// Écris une fonction qui retourne le nombre de lettres dans une chaîne (en ignorant les espaces, chiffres, ponctuations).
+
+// **************************
+
+function countChar(str: string): number {
+  if (typeof str !== "string") {
+    throw new Error("La saisie doit être une chaîne de caractères");
+  }
+
+  let count: number = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i].toUpperCase() !== str[i].toLowerCase()) {
+      count = count + 1;
+    }
+  }
+  return count;
+}
+
+console.log(countChar("Ludovic à la plage"));
+
+function countLetters(str: string): number {
+  if (typeof str !== "string") {
+    throw new Error("La saisie doit être une chaîne de caractères");
+  }
+  let count: number = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (/\p{L}/u.test(str[i])) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(countLetters("Ludovic à la Plage"));
