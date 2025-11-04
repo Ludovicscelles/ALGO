62e85ff9361b94d778b79a2b16ad6cb92c8272d5
Ludovicscelles <161328869+Ludovicscelles@users.noreply.github.com>
2025-11-04 18:14:06 +0100

    index10: revise function sumOfDiggitsEvenNumbers2 using a while loop

--- dist/index10.js
+++ dist/index10.js
@@ -21,3 +21,27 @@ function sumOfDiggitsEvenNumbers(NB) {
 let NB = 10024;
 let sumEvenNum = sumOfDiggitsEvenNumbers(NB);
 console.log(sumEvenNum);
+// *******************************
+// 🔢 1bis. Somme des chiffres pairs
+// Écris une fonction qui prend un nombre entier et retourne la somme de ses chiffres pairs.
+// 📌 Exemple : 2489 → 2 + 4 + 8 = 14
+// *******************************
+function sumOfDiggitsEvenNumbers2(num01) {
+    if (!Number.isInteger(num01)) {
+        throw new Error(`La saisie doit être un nombre entier`);
+    }
+    let sum = 0;
+    let numString = num01.toString();
+    let i = 0;
+    while (i < numString.length) {
+        let digit = parseInt(numString[i]);
+        if (digit % 2 === 0) {
+            sum += digit;
+        }
+        i++;
+    }
+    return sum;
+}
+let num01 = 1024;
+let sumEvenNum1 = sumOfDiggitsEvenNumbers2(num01);
+console.log(sumEvenNum1);
--- index10.ts
+++ index10.ts
@@ -26,3 +26,34 @@ function sumOfDiggitsEvenNumbers(NB: number): number {
 let NB = 10024;
 let sumEvenNum: number = sumOfDiggitsEvenNumbers(NB);
 console.log(sumEvenNum);
+
+// *******************************
+
+// 🔢 1bis. Somme des chiffres pairs
+// Écris une fonction qui prend un nombre entier et retourne la somme de ses chiffres pairs.
+// 📌 Exemple : 2489 → 2 + 4 + 8 = 14
+
+// *******************************
+
+function sumOfDiggitsEvenNumbers2(num01: number): number {
+  if (!Number.isInteger(num01)) {
+    throw new Error(`La saisie doit être un nombre entier`);
+  }
+
+  let sum: number = 0;
+  let numString: string = num01.toString();
+
+  let i = 0;
+  while (i < numString.length) {
+    let digit: number = parseInt(numString[i]);
+    if (digit % 2 === 0) {
+      sum += digit;
+    }
+    i++;
+  }
+  return sum;
+}
+
+let num01: number = 1024;
+let sumEvenNum1: number = sumOfDiggitsEvenNumbers2(num01);
+console.log(sumEvenNum1);
